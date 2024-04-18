934827f3223aaf04141ab4860aaba7719fb3cfee
groovy-phazuma <phazuma19980625@gmail.com>
2024-04-19 01:23:12 +0900

    [240419 MCLUV3] update tutorial

--- tutorial/views.py
+++ tutorial/views.py
@@ -4,8 +4,18 @@ Created on 2024-04-16 (Tue) 23:14:54
 
 @author: I.Azuma
 """
+from flask import render_template
 from tutorial import app
 
-@app.route('/test')
+@app.route('/')
 def index():
-    return 'Hellow World! Test'
+    my_dict = {
+        'insert_something1': 'views.pyのinsert_something1部分です。',
+        'insert_something2': 'views.pyのinsert_something2部分です。',
+        'test_titles':['title1','title2','title3']
+    }
+    return render_template('tutorial/index.html', my_dict=my_dict)
+
+@app.route('/test')
+def other():
+    return render_template('tutorial/index2.html')
