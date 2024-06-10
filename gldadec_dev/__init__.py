ca89cb006180ca8a02586a90651acc1f56df17a2
groovy-phazuma <phazuma19980625@gmail.com>
2024-06-10 23:53:29 +0900

    [240610 MCLUV3] done tutorial

--- gldadec_dev/__init__.py
+++ gldadec_dev/__init__.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python3
-"""
-Created on 2024-04-16 (Tue) 23:16:39
-
-@author: I.Azuma
-"""
 from flask import Flask
 
 app = Flask(__name__)
--- gldadec_dev/views.py
+++ gldadec_dev/views.py
@@ -4,8 +4,9 @@ Created on 2024-04-16 (Tue) 23:14:54
 
 @author: I.Azuma
 """
-from flask import render_template
+from flask import render_template, request
 from gldadec_dev import app
+from gldadec_dev.calc_circle import calculation_circle
 
 @app.route('/')
 def index():
@@ -19,3 +20,18 @@ def index():
 @app.route('/calc')
 def other():
     return render_template('files/calc.html')
+
+@app.route("/calc", methods=['GET','POST'])
+def calc():
+	if request.method == 'GET':
+		return render_template('files/calc.html')
+	elif request.method == 'POST':
+		diameter = request.form['diameter']
+		result = calculation_circle(diameter)
+		if len(result) == 2:
+			return render_template('files/calc.html', area=result[0],circumference=result[1])
+		else:
+			return render_template('files/calc.html',error=result)
+		
+if __name__ == '__main__':
+    app.run()
