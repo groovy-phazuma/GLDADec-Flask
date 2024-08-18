e7e6070afd3bb60215d14d5b9d88fcefa7186e99
groovy-phazuma <phazuma19980625@gmail.com>
2024-08-18 23:48:55 +0900

    [240818 MCLUV3] update upload

--- gldadec_dev/views.py
+++ gldadec_dev/views.py
@@ -5,9 +5,10 @@ Created on 2024-04-16 (Tue) 23:14:54
 @author: I.Azuma
 """
 import os
+from datetime import datetime
 import pandas as pd
 
-from flask import render_template, request
+from flask import render_template, request, make_response
 from flask_wtf import FlaskForm
 from wtforms import FileField, SubmitField
 from wtforms.validators import DataRequired
@@ -15,6 +16,8 @@ from wtforms.validators import DataRequired
 from gldadec_dev import app
 from gldadec_dev.calc_circle import calculation_circle
 from gldadec_dev.simple_run import run_simple_gldadec
+
+UPLOAD_DIR = '/tmp'
 
 @app.route('/')
 def index():
@@ -31,7 +34,7 @@ def run_simple():
 		deconv_res = run_simple_gldadec()
 		return render_template('files/simple_run.html',title=title,table=(deconv_res.to_html(classes='table table-striped')))
 
-@app.route("/calc", methods=['GET','POST'])
+@app.route('/calc', methods=['GET','POST'])
 def calc():
 	if request.method == 'GET':
 		return render_template('files/calc.html')
@@ -44,29 +47,55 @@ def calc():
 			return render_template('files/calc.html',error=result)
 
 class UploadMixture(FlaskForm):
-    file = FileField('Mixture', validators=[DataRequired()])
-    submit = SubmitField('Upload_1')
+    file = FileField('Mixture & Reference', validators=[DataRequired()])
+    submit1 = SubmitField('Upload1')
 
 class UploadReference(FlaskForm):
     file = FileField('Reference', validators=[DataRequired()])
-    submit = SubmitField('Upload_2')
+    submit2 = SubmitField('Upload2')
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
 	form1 = UploadMixture()
-	form2 = UploadReference()
 
-	if form1.validate_on_submit() and form2.validate_on_submit():
-
+	if form1.validate_on_submit():
 		file1 = form1.file.data
-		df1 = pd.read_csv(file1)
-		print(df1.head())
+		print("Mixture Path:",form1.file)
+
+		try:
+			df1 = pd.read_csv(file1)
+			print("Mixture File:")
+			print(df1.head())
+		except Exception as e:
+			print(f"Error reading Mixture file: {e}")
+		
+		try:
+			df2 = pd.read_csv(file2)
+			print("Reference File:")
+			print(df2.head())
+		except Exception as e:
+			print(f"Error reading Reference file: {e}")
 	
-		file2 = form2.file.data
-		df2 = pd.read_csv(file2)
-		print(df2.head())
-	
-	return render_template('files/upload.html', form1=form1, form2=form1)
+	return render_template('files/upload.html', form1=form1)
+
+@app.route('/upload_multi', methods=['GET', 'POST'])
+def upload_multipart():
+	"""
+	upload_files = request.files.getlist('uploadFile_aa')
+	for file in upload_files:
+		fileName = file.filename
+		print(fileName)
+		saveFileName = datetime.now().strftime("%Y%m%d_%H%M%S_") + str(fileName)
+		save_path = os.path.join(UPLOAD_DIR, saveFileName)
+		save_path = save_path.replace("\\", "/")
+		print(save_path)
+		file.save(save_path)
+	"""
+	for name in request.files:
+		fs = request.files[name]
+		fs.save('./tmp/'+fs.filename)
+
+	return render_template('files/upload_multi.html')
 
 if __name__ == '__main__':
     app.run()
